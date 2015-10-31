1e489bb8c30eb44dd3eaaa05fed01c0b17febdc7
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-10-31 12:50:09 -0600

    Created custom sidenav, most functionality has been completed

--- public/js/admin/adminCtrl.js
+++ public/js/admin/adminCtrl.js
@@ -1,5 +1,36 @@
 app.controller('adminCtrl', function($scope) {
-	$scope.isExpanded = false;
+	var custIsExpanded = false;
 	
-	
+	$scope.toggleSidenav = function() {
+		if (custIsExpanded) {
+			custIsExpanded = false;
+			
+			angular.element('.collapsed-sidenav-toolbar')
+				.css({
+					'width': '85px'
+				});
+			
+			angular.element('.menu-icon')
+				.removeClass('pull-right');
+			
+			angular.element('.open-expand')
+				.addClass('hidden');
+				
+		} else {
+			custIsExpanded = true;
+			
+			angular.element('.collapsed-sidenav-toolbar')
+				.css({
+					// 'transform': 'translatex(57px)', 
+					// 'transform-origin': 'bottom left', 
+					'width': '200px'
+				});
+			
+			angular.element('.menu-icon')
+				.addClass('pull-right');
+			
+			angular.element('.open-expand')
+				.removeClass('hidden');
+		};
+	}
 });
