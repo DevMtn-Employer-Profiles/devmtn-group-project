53a2bd5bf450b6eec4a610a6252930206a32d5e5
Tim Charlesworth <Tim Charlesworth>
2015-11-03 15:57:40 -0700

    connected devmtn auth

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,15 @@
 var mongoose = require('mongoose'),
     profile = require('../controllers/profile'),
     skill = require('../controllers/skills'),
-    notification = require('../controllers/notification');
+    notification = require('../controllers/notification'),
+    passport = require('passport'),
+    session = require('express-session');
 
 module.exports = function (app){
+  /**********Middleware*********/
+  app.use(session({secret: 'hahahhaaha'}));
+  app.use(passport.initialize());
+  app.use(passport.session());
   /**********Endpoints**********/
   //Profiles
   app.get('/api/profile',/*auth.requiresApiLogin(),*/ profile.getProfile);
@@ -18,6 +24,8 @@ module.exports = function (app){
   app.get('/api/notifications', notification.getNotifications);
   app.delete('/api/notifications/:id', notification.deleteNotification);
   app.post('/api/notifications', notification.addNotification);
+  //Authentication
+  app.post('/auth/devmtn', passport.authenticate('devmtn'));
   //Catch-all api errors
   app.all('/api/*', function(req, res){
     res.send(404);
