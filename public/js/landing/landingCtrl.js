095287193ea9e6a0de4441f01920cd1b616bb966
Jimmy <Jimmy>
2015-11-04 14:50:23 -0800

    updated landing page, it's controller and css

--- public/js/landing/landingCtrl.js
+++ public/js/landing/landingCtrl.js
@@ -1,3 +1,30 @@
-app.controller('LandingController', function() {
+app.controller('LandingController', function($scope) {
 	
+	  $(function(){
+        $(".element").typed({
+            strings: ["First sentence.", "Second sentence."],
+            typeSpeed: 0
+        });
+    });
+	
+	//typed.js function
+	  $scope.updates = ["Web Developer", "iOS Developer", "James Carrigan"];
+
+  $(function() {
+    $(".update-box").typed({
+      strings: $scope.updates,
+      typeSpeed: 40,
+      loop: true,
+      backDelay: 1500,
+      contentType: 'text',
+      loopCount: false,
+      cursorChar: " |",
+    });
+    $(".update-box").css({
+      'color': '#34ACE0',
+      'font-size': '30px',
+      'font-weight': 'bold'
+    })
+  }());
+
 });
