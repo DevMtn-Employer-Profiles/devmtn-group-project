a180e0ae69b187e714fee7e16c0d7d03bd435631
Tim Charlesworth <Tim Charlesworth>
2015-11-04 15:53:04 -0700

    tweeking server file to erase old auth endpoints

--- server/devmtn-auth-master/example/server.js
+++ server/devmtn-auth-master/example/server.js
@@ -22,49 +22,6 @@ app.use(session({
 }))
 app.use(express.static(__dirname + '/public'));
 
-//-----------------passport setup----------------
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use('devmtn', new DevmtnStrategy(devmtnAuthConfig, function(jwtoken, user, done) {
-  // Find or create a user in your database here and return your user.
-  // json web token is also provided here as jwtoken and could be added to the session for use on API calls to devmounta.in
-
-  User.findOrCreate({email: user.email}, function(err, local_user) {
-    return done(err, local_user)
-  })
-
-}))
-
-//-----------------end passport setup----------
-
-//------------------auth endpoints-------------
-
-app.get('/auth/devmtn', passport.authenticate('devmtn'), function(req, res) {
-  // redirects, so this doesn't get called
-})
-
-// failureRedirect and res.redirect should be changed to match your app
-app.get('/auth/devmtn/callback', passport.authenticate('devmtn', {failureRedirect: '/#/login'}),
-  function(req, res) {
-    //successful authentication
-
-    console.log(req.user); // req.user is created by passport from the decoded json web token
-    console.log('user roles:', req.user.roles);
-    console.log('student:', Devmtn.checkRoles(req.user, 'student')) //example of checking user roles
-
-    res.redirect('/#/dashboard')
-  });
-
-app.get('/auth/devmtn/logout', function(req, res) {
-  req.logout();
-  console.log(req.user); //just showing req.user is undefined after logout
-  res.redirect('/#/')
-})
-
-//--------------end auth endpoints ------------
-
 
 // Start server
 app.listen(port, function() {
