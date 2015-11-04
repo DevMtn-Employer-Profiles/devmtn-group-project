c9e00b0735051f61ac32ba3c17b046a83706517f
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-11-04 09:13:38 -0700

    Notification/welcome message edits are halfway done

--- public/js/admin/config/adminConfigCtrl.js
+++ public/js/admin/config/adminConfigCtrl.js
@@ -1,3 +1,21 @@
-app.controller('adminConfigCtrl', function($scope) {
+app.controller('adminConfigCtrl', function($scope, dataService) {
 	
+	$scope.notifications = [];
+	
+	(function getNotifications() {
+		dataService.getNotifications()
+			.then(function(result) {
+				$scope.notifications = result;
+				console.log(result);
+				$scope.welcomeMessage = $scope.notifications.splice(0, 1)[0].message;
+				console.log($scope.welcomeMessage);
+			});
+	})();
+	
+	
+	$scope.deleteNotification = function(index) {
+		dataService.deleteNotification($scope.notifications[index]._id);
+		
+		$scope.notifications.splice(index, 1);
+	};
 });
