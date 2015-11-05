a707782d727e4f6268231435debd2343f0bc05a0
Jimmy <Jimmy>
2015-11-05 14:06:54 -0800

    Landing page styling

--- public/js/landing/landingCtrl.js
+++ public/js/landing/landingCtrl.js
@@ -1,30 +1,23 @@
 app.controller('LandingController', function($scope) {
-	
-	  $(function(){
-        $(".element").typed({
-            strings: ["First sentence.", "Second sentence."],
-            typeSpeed: 0
-        });
-    });
-	
-	//typed.js function
-	  $scope.updates = ["Web Developer", "iOS Developer", "James Carrigan"];
+      $scope.updates = ["Web Developer", "iOS Developer"];
 
-  $(function() {
-    $(".update-box").typed({
-      strings: $scope.updates,
-      typeSpeed: 40,
-      loop: true,
-      backDelay: 1500,
-      contentType: 'text',
-      loopCount: false,
-      cursorChar: " |",
-    });
-    $(".update-box").css({
-      'color': '#34ACE0',
-      'font-size': '30px',
-      'font-weight': 'bold'
-    })
-  }());
+  //typed.js function
+
+//   $(function() {
+//     $(".update-box").typed({
+//       strings: $scope.updates,
+//       typeSpeed: 40,
+//       loop: true,
+//       backDelay: 1500,
+//       contentType: 'text',
+//       loopCount: false,
+//       cursorChar: " |",
+//     });
+//     $(".update-box").css({
+//       'color': '#34ACE0',
+//       'font-size': '30px',
+//       'font-weight': 'bold'
+//     })
+//   }());
 
 });
