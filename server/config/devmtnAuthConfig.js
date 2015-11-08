cb6a511f2973dc23aaa01d3a4708183b28624cee
Doug Irwin <me@dougirw.in>
2015-11-08 00:30:04 -0700

    DevMtn Config.js found on master and deleted
    Sorry I did this without others looking at this first.

--- server/config/devmtnAuthConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	app: 'dev-ep',
-	client_token: 'ZKoOYXQzb8fYIoSsSDrS8Wdi7rpUASm36LgBh',
-	callbackURL: 'http://localhost:3000/auth/devmtn/callback',
-	// callbackURL: 'http://profiles.devmounta.in/auth/devmtn/callback',  **Development Callback?
-	jwtSecret: 'hashtagDevmountinIsAweseomeHashtagLearnToCode'
-}
