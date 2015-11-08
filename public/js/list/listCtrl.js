29562fc3f15d86c27e6878921aa4ba3fe3bc99b8
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-11-08 16:28:38 -0700

    Updates to the list view

--- public/js/list/listCtrl.js
+++ public/js/list/listCtrl.js
@@ -2,10 +2,13 @@ angular.module('MainApp').controller('listCtrl', function($scope, dataService) {
 	$scope.profiles = [];	
 	var getAllCompanies = function() {
 		dataService.getActiveCompanies().then(function(resp) {
-			console.log("Got Active: ", resp);
 			$scope.profiles = resp;
 		});		
 	}
 	
 	getAllCompanies();
+	
+	$scope.search = function(company) {
+		return (angular.lowercase(company.companyName).indexOf(angular.lowercase($scope.query) || '') !== -1);
+	};
 });
