24f080bc59eeb4dc89217dad8e0c9d1f50452327
Tim Charlesworth <tim_charlesworth@outlook.com>
2015-11-18 10:42:35 -0700

    finished student page

--- public/js/authService/authService.js
+++ public/js/authService/authService.js
@@ -1,45 +1,39 @@
-angular.module('MainApp').service('authService', function($http, $q) {
+angular.module('MainApp').service('authService', function($http, $q, $state) {
 
 	var simpleLoginReturn = function(result) {
-		//save user to local storage
-		localStorage.setItem('devmtnUser', result);
 		return result.data;
 	}
 	
 	var loginErrorHandler = function(error) {
-		console.error(error);
+		if(error.status === 401) {
+			//unauthorized
+			$state.go('Landing');
+		} else {
+			console.error(error);
+		}
 	}
 	
-	//This feels a little buggy, probably won't work --Tim
 	this.getCurrentUser = function() {
-		var deferred = $q.defer();
-
-		//check server for the current user
-		$http({
+		return $http({
 			method: 'GET',
 			url: '/auth/currentUser'
-		})
-			.then(function(response) {
-				deferred.resolve(response.data);
-			});
-		
-		return deferred.promise;
+		}).then(simpleLoginReturn, loginErrorHandler);
 	}
 	
 	this.login = function() {
 		return $http({
 			method: 'GET',
 			url: '/auth/devmtn'
-		}).then(function(success){console.log("SUCCESS", success)}, function(err){console.error('ERROR',err)});
+		}).then(simpleLoginReturn, loginErrorHandler);
 	}
 	
 	this.logout = function() {
-		//clear from localstorage
-		localStorage.removeItem('devmtnUser');
 		//logout on server
-		$http({
+		return $http({
 			method: 'GET',
 			url: '/auth/logout'
-		})
+		}).then(function(success) {
+			$state.go('Landing');
+		}, loginErrorHandler)
 	}
 });
