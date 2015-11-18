34131feccfc89c8cd52a226baf09e9c492ca5488
Tim Charlesworth <tim_charlesworth@outlook.com>
2015-11-18 12:24:28 -0700

    fixed employer button to go to match page

--- public/js/employer/students/employerStudentCtrl.js
+++ public/js/employer/students/employerStudentCtrl.js
@@ -1,17 +1,23 @@
 angular.module('MainApp').controller('employerStudentCtrl', function(dataService, $scope, $location) {
 	$scope.showMatches = false;
+	var myProfile;
 	//Check profile
 	dataService.getMyProfile().then(function(profile) {
+		console.log("FOUND: ", profile);
 		if(profile && profile.studentMatches && profile.studentMatches.length > 0) {
 			$scope.showMatches = true;
+			myProfile = profile;
 		} else {
 			$scope.showMatches = false;
+			myProfile = null;
 		}
 	})
 	
 	$scope.viewMatches = function() {
 		if($scope.showMatches) {
-			console.log("PROGRAM ME!");
+			console.log("profile: ", myProfile);
+			console.log("PROGRAM ME!", myProfile._id);
+			window.open('http://localhost:3000/#/myMatches/'+myProfile._id);
 		}
 	}
 	
